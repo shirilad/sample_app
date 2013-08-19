f96c51e86ef1b76dcd55be67bda7a2f3a6e75513
shirilad <shirilad@gmail.com>
2013-08-19 12:00:43 +0300

    modify tests a bit

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,54 +4,6 @@ describe "UserPages" do
 
 	subject { page }
 
-	describe "index" do
-
-		let(:user) { FactoryGirl.create(:user) }
-
-		before(:each) do
-			sign_in user
-			visit users_path
-		end
-
-		it { should have_title('All users') }
-		it { should have_content('All users') }
-
-		describe "pagination" do
-
-			before(:all)	{ 30.times { FactoryGirl.create(:user) } }
-			after(:all)		{ User.delete_all }
-
-			it { should have_selector('div.pagination') }
-
-			it "should list each user" do
-				User.paginate(page: 1).each do |user|
-					expect(page).to have_selector('li', text: user.name)
-				end
-			end
-		end
-
-		describe "delete links" do
-
-			it { should_not have_link('delete') }
-
-			describe "as an admin user" do
-				let(:admin) { FactoryGirl.create(:admin) }
-				before do
-					sign_in admin
-					visit users_path
-				end
-
-				it { should have_link('delete', href: user_path(User.first)) }
-				it "should be able to delete another user" do
-					expect do
-						click_link('delete', match: :first)
-					end.to change(User, :count).by(-1)
-				end
-				it { should_not have_link('delete', href: user_path(admin)) }
-			end
-		end
-	end
-
 	describe "signup page" do
 		before { visit signup_path }
 
@@ -112,7 +64,7 @@ describe "UserPages" do
 
 	describe "edit" do
 		let(:user) { FactoryGirl.create(:user) }
-		before do
+		before do 
 			sign_in user
 			visit edit_user_path(user)
 		end
@@ -143,8 +95,8 @@ describe "UserPages" do
 			it { should have_title(new_name) }
 			it { should have_selector('div.alert.alert-success') }
 			it { should have_link('Sign out', href: signout_path) }
-			specify { expect(user.reload.name).to	eq new_name }
-			specify { expect(user.reload.email).to	eq new_email }
+			specify { expect(user.reload.name).to eq new_name }
+			specify { expect(user.reload.email).to eq new_email }
 		end
 	end
 end
